bacf70dc51cf47bf5538acb4af005e929f0e2d18
Isaac Borges <isaacbmota@gmail.com>
2020-03-19 11:06:39 -0300

    Improve validations

--- public/scripts.js
+++ public/scripts.js
@@ -14,16 +14,33 @@ document
             .classList.toggle('hide')
     })
 
+document
+    .querySelector('input#nao')
+    .addEventListener("click", function(){
+        document
+            .querySelector('section.check')
+            .classList.toggle('hide')
+    })
+    
+document.addEventListener("DOMContentLoaded", function() {
+    var elements = document.getElementsByTagName("INPUT");
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].oninvalid = function(e) {
+            e.target.setCustomValidity("");
+            if (!e.target.validity.valid) {
+                e.target.setCustomValidity("Inválido! Preencher no seguinte formato: 61999999999");
+            }
+        };
+        elements[i].oninput = function(e) {
+            e.target.setCustomValidity("");
+        };
+    }
+})
+    
 const formValidation = () => { 
         if (document.querySelector('input#name').value === '') {
             alert('Preencha o campo com o seu nome e sobrenome');
             document.querySelector('input#name').focus();
             return false;
         }
-
-        if (document.querySelector('input#telefone').value === '') {
-            alert('Preencha o campo com o seu número de telefone');
-            document.querySelector('input#telefone').focus()
-            return false;
-        }
-    }
+}
